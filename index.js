5fb5642b24811beae326ebc815a4df722c0e6386
scout.burghardt@gmail.com <scout.burghardt@gmail.com>
2018-05-17 12:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function johnLennonFacts(array) {
     array[i] += "!!!";
     i++;
   }
+<<<<<<< HEAD
   return array;
 }
 
@@ -24,3 +25,9 @@ function iLoveTheBeatles(n) {
     return myArray
   }
 }
+=======
+  console.log(array);
+}
+
+function iLoveTheBeatles()
+>>>>>>> 018546db83e28c20eafdf43f7667317111f21dfa
